27ccc864b38e4b75004d77d672cd71556e0a7513
Andrei Serguienko <andrei.serguienko@etu.univ-amu.fr>
2019-04-17 22:26:44 +0100

    Finish Dialog

---
+++ Assets/Hint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player" && other.gameObject.GetComponent<ColorScript>().color != "green")
+        {
+            gameObject.GetComponent<DialgoueTrigger>().TriggerDialogue();
+
+        }
+    }
+}
