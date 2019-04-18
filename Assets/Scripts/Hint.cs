b8f3e40e5aec4a0e27134abfb11d5fbb071fb869
Andrei Serguienko <andrei.serguienko@etu.univ-amu.fr>
2019-04-18 23:31:38 +0100

    new door hint

--- Assets/Scripts/Hint.cs
+++ Assets/Scripts/Hint.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Hint : MonoBehaviour
 {
+    public string wrongColor;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Player" && other.gameObject.GetComponent<ColorScript>().color != "green")
+        if (other.gameObject.name == "Player" && other.gameObject.GetComponent<ColorScript>().color != wrongColor)
         {
             gameObject.GetComponent<DialgoueTrigger>().TriggerDialogue();
 
